15301c1b2d2151ebf7cf0b8a343e5cc8f1a3abfa
Ilya Sitnikov <sitnikovik@gmail.com>
2026-01-19 07:39:03 +0300

    feat(commit): hash
    Implemented git commit hash and covered with tests

---
+++ commit/hash.go
@@ -0,0 +1,29 @@
+package commit
+
+// Hash represents a commit hash in the version control system.
+//
+// It is typically a SHA-1 hash that uniquely identifies a commit.
+type Hash string
+
+// NewHash creates a new Hash instance.
+func NewHash(value string) Hash {
+	return Hash(value)
+}
+
+// String returns the string representation of the commit hash.
+func (h Hash) String() string {
+	return string(h)
+}
+
+// ShortString returns a shortened version of the commit hash.
+func (h Hash) ShortString() string {
+	if len(h) < 7 {
+		return string(h)
+	}
+	return string(h)[:7]
+}
+
+// Empty checks if the commit hash is empty.
+func (h Hash) Empty() bool {
+	return len(h) == 0
+}
