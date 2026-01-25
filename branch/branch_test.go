5fe628446de67ec1d58a2ec7dba68bdefa1f8e7b
Ilya Sitnikov <sitnikovik@gmail.com>
2026-01-25 09:09:55 +0300

    test(branch): fix fmt

--- branch/branch_test.go
+++ branch/branch_test.go
@@ -82,9 +82,8 @@ func TestBranch_String(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
-			got := branch.
-				NewBranch(tt.fields.name).
-				String()
+			b := branch.NewBranch(tt.fields.name)
+			got := b.String()
 			assert.Equal(t, tt.want, got)
 		})
 	}
