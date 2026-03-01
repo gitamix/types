f7a968d81211db2991e26bdc718cd08d3a08603b
Ilya Sitnikov <31476786+sitnikovik@users.noreply.github.com>
2026-03-01 09:05:47 +0300

    feat(branch): ticket from name (#40)
    Added `Ticket()` parses ticket from branch name

--- branch/name.go
+++ branch/name.go
@@ -1,6 +1,11 @@
 package branch
 
-import "strings"
+import (
+	"regexp"
+	"strings"
+
+	"github.com/gitamix/types/ticket"
+)
 
 // Name represents the name of a git branch.
 type Name string
@@ -18,4 +23,18 @@ func (n Name) Empty() bool {
 // String returns the string representation of the branch name.
 func (n Name) String() string {
 	return string(n)
+}
+
+// Ticket extracts a Ticket from the branch name using the provided regular expression.
+//
+// The regular expression should contain a capturing group that matches the ticket name.
+// If the branch name is empty or does not match the regular expression, an empty Ticket is returned.
+//
+// Example usage:
+//
+//	re := regexp.MustCompile(`^(?:feature|bugfix|hotfix)/([A-Z]+-\d+)`)
+//	t := NewName("feature/TASK-1234").Ticket(re)
+//	fmt.Println(t.Name()) // Output: TASK-1234
+func (n Name) Ticket(re *regexp.Regexp) ticket.Ticket {
+	return ticket.ParseTicket(n.String(), re)
 }
